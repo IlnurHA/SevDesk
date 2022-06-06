0e0eed6d2a610e6c84c906873690c17a89cb0cc0
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2022-06-06 15:24:29 +0300

    fs_lib update
    Added:
    	function to merge two paths
    	function to list all files in directory
    	function to create firectories
    	function to copy files
    	function to copy directories and anything inside it

--- src/fs_lib.rs
+++ src/fs_lib.rs
@@ -1,8 +1,58 @@
-use std::ptr::metadata;
+use std::fs;
 use std::fs::*;
 
-fn create_dir(path: &str){
-    if metadata(path).is_err(){
+// Create dir
+// Wrapping function to add some checks if it will be necessary
+fn create_dir(path: &str) -> Result<(), ()> {
+    if metadata(path).is_err() {
         create_dir_all(path);
     }
+    Ok(())
+}
+
+// Copy file
+// Wrapping function
+fn copy_file(from_path: &str, to_path: &str) -> Result<(), ()> {
+    if let Err(_) = fs::copy(from_path, to_path){
+        return Err(());
+    }
+    Ok(())
+}
+
+// List all files in directory
+pub fn files_in_dir(path_to_dir: &str) -> Result<Vec<DirEntry>, ()> {
+    if let Ok(files) = read_dir(path_to_dir) {
+        let mut files_entries = files.map(|x| x.expect("Cannot read file")).collect();
+        return Ok(files_entries);
+    }
+    Err(())
+}
+
+// function to join to paths
+fn path_join<'a>(path1: &'a str, path2: &'a str) -> String {
+    let mut new_path = path1.to_owned();
+    new_path += "//";
+    new_path += path2;
+    return new_path;
+}
+
+// function to copy directory and all files and directories inside it
+pub fn copy_dir<'a>(from_path: &'a str, to_path: &'a str, dir_name: &'a str) -> Result<(), ()> {
+    let mut new_path = path_join(to_path, dir_name);
+    if create_dir(new_path.trim()).is_err() {
+        return Err(());
+    }
+
+    if let Ok(files) = files_in_dir(from_path) {
+        for file in files {
+            let filename = file.file_name().into_string().expect("Failed to read filename");
+            let filetype = file.file_type().expect("Failed to read filetype");
+            if filetype.is_file() {
+                copy_file(path_join(from_path, filename.trim()).trim(), path_join(new_path.trim(), filename.trim()).trim());
+            } else if filetype.is_dir() {
+                copy_dir(path_join(from_path, filename.trim()).trim(), new_path.trim(), filename.trim());
+            } else { return Err(()); }
+        }
+    }
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,20 @@
+mod fs_lib;
+
 use std::fs::*;
 use std::{fs, io};
 use std::io::{Read, Write};
 
+// to handle console commands
 fn read_line() -> String {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer);
     buffer
 }
 
-fn set_up_default_directories() -> Result<(), ()> {
-    if metadata("C://rust").is_err() || metadata("C://rust//default").is_err() {
-        create_dir_all("C://rust//default").expect("Cannot create an directory?");
-    }
-    Ok(())
-}
-
-fn copy_files_recursive(path1: &str, path2: &str) {
-    for file in fs::read_dir(path1).expect("Hello"){
-        println!("{}", file.unwrap().path().display());
-    }
-}
-
 fn main() {
-    // let mut file: File;
-    // if metadata("settings.txt").is_err() {
-    //     file = File::new("settings.txt");
-    //     println!("Please enter the path to the desktop");
-    //     file.write_all(read_line().as_bytes());
-    // } else {
-    //     file = File::open("settings.txt").expect("I cannot open file of settings!");
-    // }
-    //
-    // desktop_path = file.read_to_string().expect("I cannot read the setting file!");
-    // set_up_default_directories();
-    //
-    // println!("Hello, world!");
-    copy_files_recursive("C://Users//Ilnur//hs//hw//trafficLights");
+    // testing copy function
+    // It's actually working!!!
+    let path1 = "C://Users//Ilnur//OneDrive - АНО ВО Университет Иннополис//Рабочий стол//UCH//English//engPoster";
+    let path2 = "C://rust//initial";
+    fs_lib::copy_dir(path1, path2, "default");
 }
