aeaeff63e34bbd3bf9558971fb4cb0bf80b32540
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2022-06-07 07:35:41 +0300

    fs_lib changings
    All paths now represented as Path not as strings

--- src/fs_lib.rs
+++ src/fs_lib.rs
@@ -1,10 +1,11 @@
 use std::fs;
 use std::fs::*;
+use std::path::{Path, PathBuf};
 
 // Create dir
 // Wrapping function to add some checks if it will be necessary
-fn create_dir(path: &str) -> Result<(), ()> {
-    if metadata(path).is_err() {
+fn create_dir(path: &Path) -> Result<(), ()> {
+    if metadata(path.to_str().expect("Invalid Unicode")).is_err() {
         create_dir_all(path);
     }
     Ok(())
@@ -12,15 +13,15 @@ fn create_dir(path: &str) -> Result<(), ()> {
 
 // Copy file
 // Wrapping function
-fn copy_file(from_path: &str, to_path: &str) -> Result<(), ()> {
-    if let Err(_) = fs::copy(from_path, to_path){
+fn copy_file(from_path: &Path, to_path: &Path) -> Result<(), ()> {
+    if let Err(_) = fs::copy(from_path.to_str().expect("Invalid Unicode"), to_path.to_str().expect("Invalid Unicode")){
         return Err(());
     }
     Ok(())
 }
 
 // List all files in directory
-pub fn files_in_dir(path_to_dir: &str) -> Result<Vec<DirEntry>, ()> {
+pub fn files_in_dir(path_to_dir: &Path) -> Result<Vec<DirEntry>, ()> {
     if let Ok(files) = read_dir(path_to_dir) {
         let mut files_entries = files.map(|x| x.expect("Cannot read file")).collect();
         return Ok(files_entries);
@@ -29,28 +30,27 @@ pub fn files_in_dir(path_to_dir: &str) -> Result<Vec<DirEntry>, ()> {
 }
 
 // function to join to paths
-fn path_join<'a>(path1: &'a str, path2: &'a str) -> String {
-    let mut new_path = path1.to_owned();
-    new_path += "//";
-    new_path += path2;
-    return new_path;
+fn path_join<'a>(path1: &'a Path, path2: &'a Path) -> PathBuf {
+    return path1.join(path2);
 }
 
 // function to copy directory and all files and directories inside it
-pub fn copy_dir<'a>(from_path: &'a str, to_path: &'a str, dir_name: &'a str) -> Result<(), ()> {
-    let mut new_path = path_join(to_path, dir_name);
-    if create_dir(new_path.trim()).is_err() {
+pub fn copy_dir<'a>(from_path: &'a Path, to_path: &'a Path, dir_name: &'a Path) -> Result<(), ()> {
+    let mut new_pathbuf = path_join(to_path, dir_name);
+    let mut new_path = new_pathbuf.as_path();
+    if create_dir(new_path).is_err() {
         return Err(());
     }
 
     if let Ok(files) = files_in_dir(from_path) {
         for file in files {
-            let filename = file.file_name().into_string().expect("Failed to read filename");
+            let filename_s = file.file_name().into_string().expect("Failed to read filename");
+            let filename = Path::new(filename_s.trim());
             let filetype = file.file_type().expect("Failed to read filetype");
             if filetype.is_file() {
-                copy_file(path_join(from_path, filename.trim()).trim(), path_join(new_path.trim(), filename.trim()).trim());
+                copy_file(path_join(from_path, filename).as_path(), path_join(new_path, filename).as_path());
             } else if filetype.is_dir() {
-                copy_dir(path_join(from_path, filename.trim()).trim(), new_path.trim(), filename.trim());
+                copy_dir(path_join(from_path, filename).as_path(), new_path, filename);
             } else { return Err(()); }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod fs_lib;
 use std::fs::*;
 use std::{fs, io};
 use std::io::{Read, Write};
+use std::path::Path;
 
 // to handle console commands
 fn read_line() -> String {
@@ -14,7 +15,7 @@ fn read_line() -> String {
 fn main() {
     // testing copy function
     // It's actually working!!!
-    let path1 = "C://Users//Ilnur//OneDrive - АНО ВО Университет Иннополис//Рабочий стол//UCH//English//engPoster";
-    let path2 = "C://rust//initial";
-    fs_lib::copy_dir(path1, path2, "default");
+    let path1 = Path::new("C://Users//Ilnur//OneDrive - АНО ВО Университет Иннополис//Рабочий стол//UCH//English//engPoster");
+    let path2 = Path::new("C://rust//initial");
+    fs_lib::copy_dir(path1, path2, Path::new("default"));
 }
