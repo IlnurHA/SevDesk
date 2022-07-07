98411efbc9467db697612a58465ac00eb15dbe6d
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2022-07-07 11:13:26 +0300

    Refactor of existing code; -create_dir function

--- src/fs_lib.rs
+++ src/fs_lib.rs
@@ -2,44 +2,31 @@ use std::fs;
 use std::fs::*;
 use std::path::{Path, PathBuf};
 
-// Create dir
-// Wrapping function to add some checks if it will be necessary
-fn create_dir(path: &Path) -> Result<(), ()> {
-    if metadata(path.to_str().expect("Invalid Unicode")).is_err() {
-        create_dir_all(path);
-    }
+/// Copy file
+/// Wrapping function
+fn copy_file(from_path: &Path, to_path: &Path) -> std::io::Result<()> {
+    fs::copy(from_path.to_str()?, to_path.to_str()?)?;
     Ok(())
 }
 
-// Copy file
-// Wrapping function
-fn copy_file(from_path: &Path, to_path: &Path) -> Result<(), ()> {
-    if let Err(_) = fs::copy(from_path.to_str().expect("Invalid Unicode"), to_path.to_str().expect("Invalid Unicode")){
-        return Err(());
-    }
-    Ok(())
+/// List all files in directory
+pub fn files_in_dir(path_to_dir: &Path) -> std::io::Result<Vec<DirEntry>> {
+    read_dir(path_to_dir)?.collect()
 }
 
-// List all files in directory
-pub fn files_in_dir(path_to_dir: &Path) -> Result<Vec<DirEntry>, ()> {
-    if let Ok(files) = read_dir(path_to_dir) {
-        let mut files_entries = files.map(|x| x.expect("Cannot read file")).collect();
-        return Ok(files_entries);
-    }
-    Err(())
-}
-
-// function to join to paths
+/// function to join to paths
 fn path_join<'a>(path1: &'a Path, path2: &'a Path) -> PathBuf {
     return path1.join(path2);
 }
 
-// function to copy directory and all files and directories inside it
-pub fn copy_dir<'a>(from_path: &'a Path, to_path: &'a Path, dir_name: &'a Path) -> Result<(), ()> {
+/// function to copy directory and all files and directories inside it
+pub fn copy_dir<'a>(from_path: &'a Path, to_path: &'a Path, dir_name: &'a Path, black_list: &[Path]) -> std::io::Result<()> {
     let mut new_pathbuf = path_join(to_path, dir_name);
     let mut new_path = new_pathbuf.as_path();
-    if create_dir(new_path).is_err() {
-        return Err(());
+    create_dir_all(new_path)?;
+
+    if black_list.contains(new_path) {
+        return Ok(());
     }
 
     if let Ok(files) = files_in_dir(from_path) {
@@ -47,10 +34,15 @@ pub fn copy_dir<'a>(from_path: &'a Path, to_path: &'a Path, dir_name: &'a Path) 
             let filename_s = file.file_name().into_string().expect("Failed to read filename");
             let filename = Path::new(filename_s.trim());
             let filetype = file.file_type().expect("Failed to read filetype");
+
+            if black_list.contains(path_join(from_path, filename)) {
+                continue;
+            }
+
             if filetype.is_file() {
                 copy_file(path_join(from_path, filename).as_path(), path_join(new_path, filename).as_path());
             } else if filetype.is_dir() {
-                copy_dir(path_join(from_path, filename).as_path(), new_path, filename);
+                copy_dir(path_join(from_path, filename).as_path(), new_path, filename, black_list);
             } else { return Err(()); }
         }
     }
